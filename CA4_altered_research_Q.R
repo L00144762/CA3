6120cccba74a648028a071ef77abb397def34ade
unknown <L00144762@student.lyit.ie>
2019-05-29 16:44:08 +0100

    finished -

--- CA4_altered_research_Q.R
+++ CA4_altered_research_Q.R
@@ -12,7 +12,7 @@ Ulster_test <- slice(Ulster, 1:(n()-3))
 
 ts_Ulster_Temp <- ts(Ulster_test$`Malin Head Temp`, start = c(1990,1),
                      frequency = 12)
-
+ts_Ulster_Temp
 plot(ts_Ulster_Temp, main = "Raw time series")
 par(resetPar())
 # there seems to be some seasonal effect and random fluctuations
@@ -75,7 +75,7 @@ seasonplot(ts_Ulster_Temp, 12, col=rainbow(12),
 
 seasonplot(ulster_seasonadj, 12, col=rainbow(12),
            year.labels =  TRUE,
-           main = "seasonal element removed from ulster temp")
+           main = "seasonal adjusted ulster temp")
 
 ndiffs(ulster_seasonadj)
 # d value will be 0
@@ -114,6 +114,15 @@ plot(ts_Ulster_Temp)
 # after reading met erieann weather report for 2013 September,
 # it appears there was no obvious cause to the outlier (low temperature)
 # this is likely an error in the data, and not caused by some unseen variable
+
+# replacing the outlier with the median value; distribution is not normally
+# distributed
+
+## Ulster_test$`Malin Head Temp`[285] = median(Ulster$`Malin Head Temp`)
+# this was done and it made no difference to the quality of the predictions.
+#
+
+
 
 ############ CONNACHT #################
 
@@ -172,7 +181,7 @@ seasonplot(ts_Connacht_Temp, 12, col = rainbow(12),
 
 seasonplot(connacht_seasonadj, 12, col = rainbow(12),
            year.labels = TRUE, 
-           main = "seasonal plot of connacht temp")
+           main = "seasonal adjusted plot of connacht temp")
 #massive outlier again for this dataset. this time in december 2010
 # this will effect the prediction
 ts_Connacht_Temp
@@ -205,8 +214,92 @@ plot(forecast(aa_connacht, 3))
 # which states 2009/2010 was the coldest winter in Ireland since the 1960's
 # this explains the low temperature for the months of jan and feb
 # the major outlier in december of 2010 is explained by met eireann
-# Mullingar had the coldest recorded temperature in over 50 years
+# Mullingar had the coldest recorded temperature in over 50 years (Connacht)
 
 #### Munster #####
+ 
+Munster_test <-  slice(Munster, 1:(n()-3))
+
+ts_Munster_temp <- ts(Munster_test$`Valentia Temp`, 
+                      start = c(1990,1), frequency = 12)
+
+plot(ts_Munster_temp, main = "Raw time series")
+par(resetPar())
+# there seems to be some seasonal effect and random fluctuations
+# but no overall trend which is good 
+ts_Munster_temp
+
+
+adf.test(ts_Munster_temp)
+# p value less than 0.05: time series is stationary
+# and there is no trend
+
+ndiffs(ts_Munster_temp)
+# confirms that nothing needs to be done to the series
+# and that it is stationary i.e series does not need
+# to be differenced
+par(mfrow = c(2,1))
+Pacf(ts_Munster_temp, main = "partial auto correlation plot for Munster temp")
+Acf(ts_Munster_temp, main = "auto correlation plot for Munster temp")
+par(resetPar())
+#there are loads of different p and q values that can be chosen,
+# best to use auto ARIMA instead of cycling through each
+# set manually
+
+#data has to be seasonally decomposed first
+# take the log of the series to convert from multiplicative
+# to additive
+
+log_Munster <- log(ts_Munster_temp)
+plot(log_Munster)
+# stl function to dempose the series
+# "period" so the seasonal components remain the same
+# across each year
+season_decomp_munster <- stl(log_Munster, s.window = "period")
+plot(season_decomp_munster)
+# components to be reverted back to original metric
+# take the exponential of the logged series
+
+ts_munster_converted <- exp(season_decomp_munster$time.series)
+plot(ts_munster_converted)
+# example (Ulster): sep 2017 saw a 37% seasonal change 
+
+# seasonadj removes the seasonalitiy
+munster_seasonadj <- seasadj(season_decomp_munster)
+
+seasonplot(ts_Munster_temp, 12, col = rainbow(12),
+           year.labels = TRUE, 
+           main = "seasonal plot of Munster temp")
+
+seasonplot(munster_seasonadj, 12, col = rainbow(12),
+           year.labels = TRUE, 
+           main = "seasonal adjusted plot of Munster temp")
+#massive outlier again for this dataset. Again  in december 2010
+# this will effect the prediction
+
+
+ndiffs(munster_seasonadj)
+# no differencing needed
+
+adf.test(munster_seasonadj)
+# p value < 0.05
+# good model fit 
+
+aa_munster <- auto.arima(munster_seasonadj)
+aa_munster
+
+qqnorm(aa_munster$residuals)
+qqline(aa_munster$residuals)
+# the outlier has caused problems
+# need to sort it out
+# replace this value with median/mean
+# depending on the distribution
+
+accuracy(aa_munster)
+# mape of 19%
+
+forecast(aa_munster, 3)
+plot(forecast(aa_munster, 3))
+
 
 
